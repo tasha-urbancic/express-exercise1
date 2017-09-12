d16b37e2a543440efb44afaa4ad64cef1aa14752
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-12 19:23:08 +0000

    added views and pubic directories, added EJS template in views/

--- express_server.js
+++ express_server.js
@@ -2,9 +2,9 @@
 
 const express = require("express");
 const app = express();
-app.set("view engine", "ejs");
-
 const PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
