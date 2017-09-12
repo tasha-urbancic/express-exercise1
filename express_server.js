978146c553c19c6b303ef4f9b833f789a6196055
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-12 19:51:58 +0000

    added basic html setup to urls_index.ejs, and the content of the URL database in format of a list, and a link to shorten the url

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -15,13 +15,18 @@ app.get("/", (request, response) => {
   response.end("Hello!");
 });
 
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
+app.get("/urls", (request, response) => {
+  let templateVars = { urls: urlDatabase };
+  response.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/urls.json", (request, response) => {
+//   response.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
